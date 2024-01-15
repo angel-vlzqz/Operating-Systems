33133f553404eb53773fc2aff913f540d1d01ff8
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 00:44:49 -0700

    whitespace

--- classSnippets/osStates/osStates.cpp
+++ classSnippets/osStates/osStates.cpp
@@ -19,45 +19,45 @@ int state_machine()
     {
         switch (state)
         {
-        case BLOCKED:
-        {
-            printf("I am in BLOCKED state\n");
-            state = IDLE;
-        }
-        break;
-        case READY:
-        {
-            printf("I am in the READY state\n");
-            state = IDLE;
-            break;
-        }
-        case RUN:
-        {
-            printf("I am in the RUN state\n");
-            state = IDLE;
-            break;
-        }
-        case IDLE:
-        {
-            printf("Enter state selection: 1 (BLOCKED), 2 (READY), 3 (RUN) ");
-            scanf("%d", &value);
-            switch (value)
-            {
-            case 1:
-                state = BLOCKED;
+            case BLOCKED:
+                {
+                    printf("I am in BLOCKED state\n");
+                    state = IDLE;
+                }
                 break;
-            case 2:
-                state = READY;
+            case READY:
+                {
+                    printf("I am in the READY state\n");
+                    state = IDLE;
+                    break;
+                }
+            case RUN:
+                {
+                    printf("I am in the RUN state\n");
+                    state = IDLE;
+                    break;
+                }
+            case IDLE:
+                {
+                    printf("Enter state selection: 1 (BLOCKED), 2 (READY), 3 (RUN) ");
+                    scanf("%d", &value);
+                    switch (value)
+                    {
+                        case 1:
+                            state = BLOCKED;
+                            break;
+                        case 2:
+                            state = READY;
+                            break;
+                        case 3:
+                            state = RUN;
+                            break;
+                        default:
+                            exit(0);
+                            break;
+                    }
+                }
                 break;
-            case 3:
-                state = RUN;
-                break;
-            default:
-                exit(0);
-                break;
-            }
-        }
-        break;
         }
     }
     return 1;
