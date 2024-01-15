6d4d3059714faebffc5c79649ae572911d0b71b2
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 00:40:05 -0700

    add lab question 1-2

--- classSnippets/helloWorld/helloWorld.cpp
+++ classSnippets/helloWorld/helloWorld.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+// #include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << "Hello, World!" << endl;
+    vector<int> g1;
+
+    for (int i = 0; i <= 5; i++)
+    {
+	    g1.push_back(i);
+    }
+    cout << g1[2] << endl;
 }
