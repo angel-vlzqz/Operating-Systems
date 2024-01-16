9d08702e8dbc827cc7bf5d9c90a8500e9eb84fb7
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-15 22:22:53 -0700

    whitespace

--- classSnippets/helloWorld/helloWorld.cpp
+++ classSnippets/helloWorld/helloWorld.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
-    vector<int> g1;
+  cout << "Hello, World!" << endl;
+  vector<int> g1;
 
-    for (int i = 0; i <= 5; i++)
-    {
-	    g1.push_back(i);
-    }
-    cout << g1[2] << endl;
+  for (int i = 0; i <= 5; i++)
+  {
+    g1.push_back(i);
+  }
+  cout << g1[2] << endl;
 }
